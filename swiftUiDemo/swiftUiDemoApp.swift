336a425ccc31450ef7abad58f1945ea96daf2977
mohamedEldeeb13 <mooel744@gmail.com>
2023-12-11 16:44:11 +0200

    Initial Commit

---
+++ swiftUiDemo/swiftUiDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  swiftUiDemoApp.swift
+//  swiftUiDemo
+//
+//  Created by Mohamed Abd Elhakam on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
